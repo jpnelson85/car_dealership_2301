4b31430e07fa26631e2230d1b36748c846a9aecd
Jeff Nelson <jpnelson85@gmail.com>
2023-02-08 11:36:43 -0600

    add second test on dealership_sec

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,7 +1,13 @@
 class Dealership
-
+  attr_reader :company, :address, :inventory, :inventory_count
+  
   def initialize(company, address)
+    @company = company
+    @address = address
+    @inventory = []
+    @inventory_count = 0
   end
 
+  
 
 end
