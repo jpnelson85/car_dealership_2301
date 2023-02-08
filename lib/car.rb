9322b6283efb276642b82dc96bf9263db882ac92
Jeff Nelson <jpnelson85@gmail.com>
2023-02-08 11:14:46 -0600

    finish iteration 1

--- lib/car.rb
+++ lib/car.rb
@@ -1,7 +1,21 @@
 class Car
-  def initialize(make_model, monthly_payment, loan_length)
-    @make_model = make_model
-    @monthly_payment = monthly_payment
-    @loan_length = loan_length
+  attr_reader :color, :make, :model, :monthly_payment, :loan_length
+
+
+  def initialize(car, monthly_payment, loan_length)
+      @make = car.split[0]
+      @model = car.split[1]
+      @monthly_payment = monthly_payment
+      @loan_length = loan_length
+      @color = nil
   end
+
+  def total_cost
+      @loan_length * @monthly_payment
+  end
+
+  def paint!(paint_color)
+  @color = paint_color
+  end
+
 end
